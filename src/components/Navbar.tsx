6ecd87d4e1e0ace3b73ea7f6b270aa1c3e22e17f
ThienANh <thienanh.0152@yahoo.com>
2024-11-27 16:59:58 +0700

    config KINDE : login,logout,register

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,18 @@
 import MobileMenu from "@/components/MobileMenu";
 import NavbarLinks from "@/components/NavbarLinks";
 import { Button } from "@/components/ui/button";
+import UserNav from "@/components/UserNav";
+import {
+  LoginLink,
+  RegisterLink,
+} from "@kinde-oss/kinde-auth-nextjs/components";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import Link from "next/link";
 
-export default function Navbar() {
+export default async function Navbar() {
+  const { getUser } = getKindeServerSession();
+  const user = await getUser();
+
   return (
     <nav className="relative max-w-7xl w-full flex md:grid md:grid-cols-12 items-center px-4 md:px-8 mx-auto py-7">
       {/* Logo */}
@@ -20,8 +29,24 @@ export default function Navbar() {
 
       {/* Login-Register */}
       <div className="flex items-center gap-x-2 ms-auto md:col-span-3">
-        <Button>Login</Button>
-        <Button variant={"secondary"}>Register</Button>
+        {user ? (
+          <UserNav
+            email={user.email as string}
+            name={user.given_name as string}
+            userImage={
+              user.picture ?? `https://avatar.vercel.sh/${user.given_name}`
+            }
+          />
+        ) : (
+          <div className="flex gap-x-2 items-center">
+            <Button asChild>
+              <LoginLink>Login</LoginLink>
+            </Button>
+            <Button variant={"secondary"} asChild>
+              <RegisterLink>Register</RegisterLink>
+            </Button>
+          </div>
+        )}
 
         {/* Mobile Menu */}
         <div className="md:hidden">
---
+++ src/components/UserNav.tsx
@@ -0,0 +1,67 @@
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { LogoutLink } from "@kinde-oss/kinde-auth-nextjs/components";
+
+interface iAppProps {
+  email: string;
+  name: string;
+  userImage: string | undefined;
+}
+
+export default function UserNav({ email, name, userImage }: iAppProps) {
+  return (
+    <DropdownMenu>
+      {/* Button trigger */}
+      <DropdownMenuTrigger asChild>
+        <Button variant={"ghost"} className="relative h-10 w-10 rounded-full">
+          <Avatar className="h-10 w-10">
+            <AvatarImage src={userImage ?? ""} alt={name} />
+            <AvatarFallback>{name.slice(0, 3)}</AvatarFallback>
+          </Avatar>
+        </Button>
+      </DropdownMenuTrigger>
+
+      {/* Content */}
+      <DropdownMenuContent className="w-56" align="end" forceMount>
+        {/* Label */}
+        <DropdownMenuLabel className="font-normal">
+          <div className="flex flex-col space-y-1">
+            <p className="text-sm font-medium leading-none">{name}</p>
+            <p className="text-xs leading-none text-muted-foreground">
+              {email}
+            </p>
+          </div>
+        </DropdownMenuLabel>
+
+        <DropdownMenuSeparator />
+
+        {/* Menu group */}
+        <DropdownMenuGroup>
+          <DropdownMenuItem>Profile</DropdownMenuItem>
+          <DropdownMenuItem>Billing</DropdownMenuItem>
+          <DropdownMenuItem>Settings</DropdownMenuItem>
+          <DropdownMenuItem>
+            Keyboard shortcuts
+            {/* <DropdownMenuShortcut>⌘K</DropdownMenuShortcut> */}
+          </DropdownMenuItem>
+        </DropdownMenuGroup>
+
+        <DropdownMenuSeparator />
+
+        {/* Logout */}
+        <DropdownMenuItem asChild>
+          <LogoutLink>Logout</LogoutLink>
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+}
