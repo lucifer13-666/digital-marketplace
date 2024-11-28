3690bcc4b712f177af91efd92aad2ad79141afab
ThienANh <thienanh.0152@yahoo.com>
2024-11-28 16:10:13 +0700

    create user when login Kinde

---
+++ src/app/api/auth/creation/route.ts
@@ -0,0 +1,34 @@
+import prisma from "@/lib/db";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import { NextResponse } from "next/server";
+
+// router gọi khi kinde login (KINDE_POST_LOGIN_REDIRECT_URL=http://localhost:3000/api/auth/creation) , lưu data lại vào prisma
+export async function GET() {
+  const { getUser } = getKindeServerSession();
+  const user = await getUser();
+
+  if (!user || user === null || !user.id) {
+    throw new Error("No user found");
+  }
+
+  let dbUser = await prisma.user.findUnique({
+    where: {
+      id: user.id,
+    },
+  });
+
+  if (!dbUser) {
+    dbUser = await prisma.user.create({
+      data: {
+        id: user.id,
+        firstName: user.given_name ?? "",
+        lastName: user.family_name ?? "",
+        email: user.email ?? "",
+        profileImage:
+          user.picture ?? `https://avatar.vercel.sh/${user.given_name}`,
+      },
+    });
+  }
+
+  return NextResponse.redirect("http://localhost:3000");
+}
